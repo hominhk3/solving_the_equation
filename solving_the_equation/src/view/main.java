3913c9665f91a3bbb5ae177a14225a1b01187955
hominhk3 <144079318+hominhk3@users.noreply.github.com>
2023-09-29 09:02:27 +0700

    Add files via upload

---
+++ solving_the_equation/src/view/main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import controller.Manager;
+
+/**
+ *
+ * @author dell
+ */
+public class main {
+    public static void main(String[] args) {
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    Manager.superlativeEquation();
+                    break;
+                case 2:
+                    Manager.quadraticEquation();
+                    break;
+                case 3:
+                    return;
+            }
+        }
+    }
+}
